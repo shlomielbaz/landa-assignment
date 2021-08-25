6ccf6c1f2c64b567047190de614c555724578fb6
Shlomi Elbaz <shlomi@yokee.tv>
2021-08-25 08:17:51 +0300

    rename to Shell

--- LA.Web/ClientApp/src/App.tsx
+++ LA.Web/ClientApp/src/App.tsx
@@ -1,6 +1,6 @@
 ﻿import * as React from 'react';
 import { Route } from 'react-router';
-import Layout from './pages/Layout';
+import Shell from './pages/Shell';
 import Home from './pages/Home';
 import Configuration from './pages/Configuration';
 import LayoutList from './pages/LayoutList';
@@ -10,10 +10,10 @@ import '../node_modules/react-grid-layout/css/styles.css';
 import '../node_modules/react-resizable/css/styles.css';
 
 export default () => (
-    <Layout>
+    <Shell>
         <Route exact path='/' component={Home} />
         <Route exact path='/home' component={Home} />
         <Route path='/configuration' component={Configuration} />
         <Route path='/list' component={LayoutList} />
-    </Layout>
+    </Shell>
 );
